2a2850585715641800234f9f1c4a34330e416e55
Артем Сидорчук <grandeur19919@gmail.com>
2017-12-23 02:26:52 +0200

    Merge branch 'dev' of gitlab.com:nc_group_2017/NC_Ships_Game into dev
    # Conflicts:
    #	src/main/java/com/nctc2017/bean/Ship.java
    #	src/main/java/com/nctc2017/configuration/ApplicationConfig.java
    #	src/main/java/com/nctc2017/constants/Query.java
    #	src/main/java/com/nctc2017/dao/ShipDao.java
    #	src/main/java/com/nctc2017/dao/impl/PlayerDaoImpl.java
    #	src/main/java/com/nctc2017/dao/impl/ShipDaoImpl.java
    #	src/main/webapp/WEB-INF/spring-mvc-config.xml
    #	src/main/webapp/WEB-INF/web.xml
    #	src/test/java/com/nctc2017/dao/Impl/AmmoDaoImplIntegrationTest.java
    #	src/test/java/com/nctc2017/dao/Impl/PlayerDaoImplTest.java
    #	src/test/java/com/nctc2017/services/TravelServiceTest.java

--- src/test/java/com/nctc2017/dao/Impl/ShipDaoImplTest.java
+++ src/test/java/com/nctc2017/dao/Impl/ShipDaoImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
@@ -22,7 +23,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {ApplicationConfig.class})
+@WebAppConfiguration
+@ContextConfiguration(classes = { ApplicationConfig.class })
 @Transactional
 public class ShipDaoImplTest {
 
