ca0349d662d2abad3edf011e7577db0ebe9dd0a0
MarcusRofler <alekzem81295@gmail.com>
2018-01-04 12:22:31 +0200

    TavernView and TavernController

--- src/main/java/com/nctc2017/controllers/TavernController.java
+++ src/main/java/com/nctc2017/controllers/TavernController.java
@@ -1,17 +1,26 @@
 package com.nctc2017.controllers;
 
+import java.math.BigInteger;
 import java.util.*;
 
 import com.nctc2017.bean.Ship;
+import com.nctc2017.bean.ShipTemplate;
+import com.nctc2017.services.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 @Controller
 public class TavernController {
+    @Autowired
+    private ShipService shipService;
+
+
 
 
     public int sailorCost;
@@ -21,17 +30,32 @@ public class TavernController {
     public ModelAndView tavernWelcome(
             @RequestParam(value = "tavern", required = false) String city) {
         ModelAndView model = new ModelAndView();
+        BigInteger id=new BigInteger("41");
+        List<Ship> ships = new ArrayList<>();
+        ShipTemplate tship1=new ShipTemplate(new BigInteger("1"),"Qwerty",10,5,10,3,3,3);
+        Ship ship1=new Ship(tship1,new BigInteger("1"),"Qwerty", 3,2, 2);
+        ShipTemplate tship2=new ShipTemplate(new BigInteger("1"),"Qwerty",10,5,10,3,3,3);
+        Ship ship2=new Ship(tship1,new BigInteger("1"),"Qwerty", 3,2, 2);
+        ships.add(ship1);
+        ships.add(ship2);
         model.addObject("msg", "This is protected page - Only for Users!");
         model.addObject("city", city);
+        model.addObject("ships", ships);
         model.setViewName("TavernView");
         return model;
     }
 
-    @Secured("ROLE_USER")
-    public List<Ship> getListOfShips(int id, int idHash) {
-        // TODO implement here
-        return null;
-    }
+
+    /*@Secured("ROLE_USER")
+    @RequestMapping(value = "/sailors", method = RequestMethod.GET)
+    public ModelAndView getListOfShips(BigInteger id,@RequestParam(value = "HireSailors", required = false) String city) {
+        id=new BigInteger("41");
+        List<Ship> ships = shipService.getAllPlayerShips(id);
+        ModelAndView model = new ModelAndView();
+        model.addObject("city", city);
+        model.addObject("ships", ships);
+        return model;
+    }*/
 
     public int getSailorsNumber(int id, int idHash, int idShip) {
         // TODO implement here
--- src/main/webapp/WEB-INF/pages/TavernView.jsp
+++ src/main/webapp/WEB-INF/pages/TavernView.jsp
@@ -5,10 +5,48 @@
     <link href="static/css/tavern.css" rel="stylesheet" media="screen">
     <link href="static/css/general.css" rel="stylesheet" media="screen">
 </head>
+
+<script type="text/javascript">
+</script>
+
 <div align="center">
-	<h1 class="titleText">${city}</h1>
+    <h1 class="titleText">${city}</h1>
 </div>
 <body>
-	<p>Tavern</p>
+<form method="get">
+    <div align="center">
+        <c:if test="${empty ships}">
+            <table class="panel">
+             <tr align="center">
+              <td >
+                <p style="font-size:80px; color:red">You dont't have any ships</p>
+              </td>
+             </tr>
+            </table>
+        </c:if>
+        <c:if test="${not empty ships}">
+            <table class="panel">
+              <tr align="center">
+               <td>
+                <button class="button"  style="vertical-align:middle" name="HireSailors" type="submit" value="Tavern ${city}" formaction="/sailors">
+                 <span>Hire sailors</span>
+                </button>
+                </td>
+               </tr>
+               <c:forEach items = "${ships}" var = "nextShip">
+               <tr align="center">
+                <td>
+                    <p>${nextShip.curName}</p>
+                    <p>${nextShip.curHealth}</p>
+                    <p>${nextShip.curSailorsQuantity}</p>
+                    <p>${nextShip.maxSailorsQuantity}</p>
+                </td>
+               </tr>
+                </c:forEach>
+            </table>
+        </c:if>
+    </div>
+</form>
 </body>
+<a href="/city" class="logOutBottom">Return to city</a>
 </html>
